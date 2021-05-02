af763e3beaee807d63bca7d792ba4b64a52b6f20
marcos <marcosassis0@gmail.com>
2021-05-02 13:26:53 -0300

    jpeg_get_resolution for JFIF files

--- jpeg/jpeg_get_size.py
+++ jpeg/jpeg_get_size.py
@@ -1,13 +1,31 @@
+# use these:
+# jpeg_get_dimensions(img_path)
+# jpeg_get_resolution(img_path) # JFIF only
+
 import os
 import io
 
+# to check file format
+# b = f.read(11) # from the begining
+# b[0:4]!=b'\xff\xd8\xff\xe0' or b[-5:]!=b'JFIF\0'
+
 def jpeg_parse_file_signature(f):
-	b = f.read(10)
-	if not b or len(b)<10 or b[0:3]!=b'\xff\xd8\xff': # b[0:4]!=b'\xff\xd8\xff\xe0' or b[-4:]!=b'JFIF':
+	b = f.read(4)
+	if not b or len(b)<4 or b[0:3]!=b'\xff\xd8\xff':
 		raise Exception("not a JPEG file")
-	
-	block_size = b[4]*256+b[5]
-	f.seek(block_size+4)
+	return b[3]
+
+def jpeg_parse_block_size(f):
+	b = f.read(2)
+	if not b or len(b)<2:
+		raise Exception("error parsing JPEG file")
+	return b[0]*256+b[1]
+
+def jpeg_jump_block(f):
+	block_size = jpeg_parse_block_size(f)
+	if block_size<2:
+		raise Exception("error parsing JPEG file")
+	f.seek(block_size-2,os.SEEK_CUR)
 
 def jpeg_parse_marker(f):
 	b = f.read(1)
@@ -19,32 +37,32 @@ def jpeg_parse_marker(f):
 		if not b or len(b)<1:
 			raise Exception("error parsing JPEG file")
 	return b[0]
-	
-def jpeg_jump_block(f):
-	b = f.read(2)
-	if not b or len(b)<2:
-		raise Exception("error parsing JPEG file")
-	
-	block_size = b[0]*256+b[1]
-	if block_size<2:
-		raise Exception("error parsing JPEG file")
-	
-	f.seek(block_size-2,os.SEEK_CUR)
 
 def jpeg_parse_dimensions(f):
 	f.seek(3,os.SEEK_CUR)
 	b = f.read(4)
 	return b[2]*256+b[3], b[0]*256+b[1]
 
-def jpeg_get_size(img_path):
+def jpeg_get_dimensions(img_path):
 	with io.open(img_path, "rb") as f:
 		jpeg_parse_file_signature(f)
-		m = jpeg_parse_marker(f)
+		m = 0
 		while not(m>=0xc0 and m<=0xc3):
 			jpeg_jump_block(f)
 			m = jpeg_parse_marker(f)
 		else:
 			w,h = jpeg_parse_dimensions(f)
 	return w,h
+
+def jpeg_parse_resolution(f):
+	if jpeg_parse_file_signature(f) != 0xe0:
+		raise Exception("cannot parse non JFIF file yet")
+	f.seek(13)
+	b = f.read(5)
+	return b[0], b[1]*256+b[2], b[3]*256+b[4] # unit, x res, y res
+
+def jpeg_get_resolution(img_path):
+	with io.open(img_path, "rb") as f:
+		return jpeg_parse_resolution(f)
 
 # TODO tests comparing to: $ identify -format "%w %h" a.jpg
